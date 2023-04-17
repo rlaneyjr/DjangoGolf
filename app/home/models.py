7e83bbc551991f8a03282ee0d389bc4255122563
Josh Bright <josh.bright@gmail.com>
2023-04-17 14:12:05 -0700

    Added golf course model

--- app/home/models.py
+++ app/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class GolfCourse(models.Model):
+    HOLE_CHOICES = (
+        ("9", "9 Holes"),
+        ("18", "18 Holes"),
+    )
+    name = models.CharField(max_length=128)
+    hole_count = models.CharField(
+        max_length=64, choices=HOLE_CHOICES, default=HOLE_CHOICES[0][0]
+    )
+    tee_time_link = models.URLField(blank=True)
+    website_link = models.URLField(blank=True)
+    city = models.CharField(max_length=128, blank=True)
+    state = models.CharField(max_length=128, blank=True)
+    zip_code = models.CharField(max_length=128, blank=True)
