773fac36ff75f6d515a69801aaed6b01fc7c4a34
Josh Bright <josh.bright@gmail.com>
2023-04-18 08:51:27 -0700

    Added more tests

--- app/core/api/tests/test_api.py
+++ app/core/api/tests/test_api.py
@@ -32,6 +32,33 @@ class PublicUserAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload["email"])
         self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test_create_user_with_email_exists_fails(self):
+        """Test if we can create a user with an email that's already used"""
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123",
+            "first_name": "First",
+            "last_name": "Last",
+        }
+        create_user(**payload)
+        res = self.client.post(USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_fails(self):
+        """Test that we can't create a user with a short password"""
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "first_name": "First",
+            "last_name": "Last",
+        }
+
+        res = self.client.post(USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        self.assertFalse(user_exists)
 
 
 # class PrivateUserAPITests(TestCase):
