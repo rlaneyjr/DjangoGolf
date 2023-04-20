a49461ade60c5879def57243bc9bb436232bf951
Josh Bright <josh.bright@gmail.com>
2023-04-19 17:12:09 -0700

    Changed how we get debug, updated allowed host list

--- app/golf_project/settings.py
+++ app/golf_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["golf.joshbright.com", "127.0.0.1"]
 
 
 # Application definition
