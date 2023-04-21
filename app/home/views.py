4bb51b716f0a6d3efa4e5b93f707364df7085d81
Josh Bright <josh.bright@gmail.com>
2023-04-20 19:42:47 -0700

    Setup a basic home front end page

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from home import models
 
 
 def index(request):
-    return render(request, "home/index.html", {})
+    game_list = None
+    if request.user.is_authenticated:
+        game_list = models.Game.objects.filter(
+            status="active", players__in=[request.user.player]
+        )
+    return render(request, "home/index.html", {"game_list": game_list})
