3178ea00bcc492ad28640e0b425caef7cab4f320
Josh Bright <josh.bright@gmail.com>
2023-04-27 18:52:15 -0700

    [#5] Added additional check to see if user is a super user

--- app/dashboard/utils.py
+++ app/dashboard/utils.py
@@ -15,4 +15,4 @@ def create_holes_for_course(course):
 
 
 def is_admin(user):
-    return user.groups.filter(name="Admin").exists()
+    return user.is_superuser or user.groups.filter(name="Admin").exists()
