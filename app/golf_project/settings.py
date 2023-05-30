71398855328aac4a117f4543a35df60db1d1e4dd
Josh Bright <josh.bright@gmail.com>
2023-05-29 21:37:44 -0700

    Added email setting for TLS

--- app/golf_project/settings.py
+++ app/golf_project/settings.py
@@ -147,6 +147,7 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST", None)
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", None)
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", None)
 EMAIL_PORT = os.environ.get("EMAIL_PORT", None)
+EMAIL_USE_TLS = True
 
 
 # Custom user model
