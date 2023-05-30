879421d532330393d67d031678933f9d3f8f23c6
Josh Bright <josh.bright@gmail.com>
2023-05-29 20:40:27 -0700

    [#7] Added classes to fields on registration form

--- app/home/forms.py
+++ app/home/forms.py
@@ -3,5 +3,11 @@ from django.contrib.auth import get_user_model
 
 
 class CustomUserForm(RegistrationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({'class': 'form-control'})
+        self.fields["password1"].widget.attrs.update({'class': 'form-control'})
+        self.fields["password2"].widget.attrs.update({'class': 'form-control'})
+
     class Meta(RegistrationForm.Meta):
         model = get_user_model()
