d3fef8a724e430bc6775a16e9bfe634ab4c3a891
Josh Bright <josh.bright@gmail.com>
2023-05-31 21:44:42 -0700

    Added more fixtures

--- app/conftest.py
+++ app/conftest.py
@@ -25,9 +25,37 @@ def player(normal_user):
 
 
 @pytest.fixture
+def player_two(normal_user):
+    player = models.Player.objects.create(
+        name="Test Player 2",
+        added_by=normal_user
+    )
+    return player
+
+
+@pytest.fixture
 def second_player(second_user):
     player = models.Player.objects.create(
         name="Test Player",
         added_by=second_user
     )
     return player
+
+
+@pytest.fixture
+def golf_course():
+    course = models.GolfCourse.objects.create(
+        name="Test Course",
+        hole_count="9",
+    )
+    return course
+
+
+@pytest.fixture
+def golf_game(golf_course, player):
+    game = models.Game.objects.create(
+        course=golf_course,
+        holes_played="9",
+    )
+    game.players.add(player)
+    return game
