4e01a03fe70464e57ef5ba3548e74a53adb783eb
Josh Bright <josh.bright@gmail.com>
2023-05-31 21:45:14 -0700

    Updated test to ensure we are testing against a golf game

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from home import utils
+from home import models
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_get_available_players_works(normal_user, golf_game, player, player_two, second_player):
+    # get players that we can add to a game
+    available_players = utils.get_players_for_game(normal_user, golf_game)
+
+    # the game currently has one player added
+    # so we should only have one other player that we can add
+    assert len(available_players) == 1
+
+    # the available player should be our second player
+    assert available_players[0] == player_two
+
+    # check to be sure that we are looking at all three players
+    # one of these players were added by a different user
+    # it should be there, but not available for us to add to a game
+    all_player_count = models.Player.objects.all().count()
+    assert all_player_count == 3
