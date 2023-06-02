60cee9200012a2ebe2b7221e4ad92834c4a9b841
Josh Bright <josh.bright@gmail.com>
2023-06-02 16:50:02 -0700

    Added id field to user serializer

--- app/core/api/serializers.py
+++ app/core/api/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ["email", "password", "first_name", "last_name"]
+        fields = ["id", "email", "password", "first_name", "last_name"]
         extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
