2c429a0df107e91ed965eae1b6af213f4e85e319
Josh Bright <josh.bright@gmail.com>
2023-06-02 11:49:03 -0700

    Added a retrieve method for the golf course viewset

--- app/home/api/views.py
+++ app/home/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from . import serializers
 from home import models
 from rest_framework import viewsets
@@ -8,4 +9,10 @@ class GolfCourseViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = models.GolfCourse.objects.all()
         serializer = serializers.GolfCourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = models.GolfCourse.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = serializers.GolfCourseSerializer(course)
         return Response(serializer.data)
