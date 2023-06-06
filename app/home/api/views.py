06728675256efb069accbaf5621859e1767c34ec
Josh Bright <josh.bright@gmail.com>
2023-06-05 20:05:38 -0700

    Added action for adding player to game on the game viewset

--- app/home/api/views.py
+++ app/home/api/views.py
@@ -4,6 +4,7 @@ from . import serializers
 from home import models
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class GolfCourseViewSet(viewsets.ViewSet):
@@ -33,6 +34,16 @@ class GameViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = models.Game.objects.all()
         serializer = serializers.GameSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["post"])
+    def add_player_to_game(self, request, pk=None):
+        queryset = models.Game.objects.all()
+        game = get_object_or_404(queryset, pk=pk)
+        player_id = request.data.get("player")
+        player_data = get_object_or_404(models.Player, pk=player_id)
+        game.players.add(player_data)
+        serializer = serializers.GameSerializer(game, many=False)
         return Response(serializer.data)
 
 
