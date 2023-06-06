6cc023cd012bcf088dfad0350025593e4fb32b49
Josh Bright <josh.bright@gmail.com>
2023-06-05 22:12:37 -0700

    Added is authenticated permission class to various api endpoints

--- app/api/views.py
+++ app/api/views.py
@@ -5,6 +5,7 @@ from home import models
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
 
 class GolfCourseViewSet(viewsets.ViewSet):
@@ -25,6 +26,8 @@ class GameViewSet(viewsets.ViewSet):
         Note: Should only be able to list your own games
     """
 
+    permission_classes = [IsAuthenticated]
+
     def retrieve(self, request, pk=None):
         queryset = models.Game.objects.all()
         game = get_object_or_404(queryset, pk=pk)
@@ -37,7 +40,7 @@ class GameViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     @action(detail=True, methods=["post"])
-    def add_player_to_game(self, request, pk=None):
+    def add_player(self, request, pk=None):
         queryset = models.Game.objects.all()
         game = get_object_or_404(queryset, pk=pk)
         player_id = request.data.get("player")
@@ -46,16 +49,26 @@ class GameViewSet(viewsets.ViewSet):
         serializer = serializers.GameSerializer(game, many=False)
         return Response(serializer.data)
 
+    @action(detail=True, methods=["post"])
+    def remove_player(self, request, pk=None):
+        queryset = models.Game.objects.all()
+        game = get_object_or_404(queryset, pk=pk)
+        player_id = request.data.get("player")
+        player_data = get_object_or_404(models.Player, pk=player_id)
+        game.players.remove(player_data)
+        serializer = serializers.GameSerializer(game, many=False)
+        return Response(serializer.data)
+
 
 class PlayerViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
     def list(self, request):
         queryset = models.Player.objects.filter(added_by=request.user)
         serializer = serializers.PlayerSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request):
-        if not request.user.is_authenticated:
-            return HttpResponseBadRequest("You must be logged in to create players")
         serializer = serializers.PlayerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(added_by=request.user)
