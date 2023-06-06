8d0968a86087c2b45aa3d14fb2f39c2ebb737f53
Josh Bright <josh.bright@gmail.com>
2023-06-05 18:13:27 -0700

    Added authenticated tests

---
+++ app/home/api/tests/test_authenticated_api.py
@@ -0,0 +1,43 @@
+import pytest
+from django.shortcuts import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from home import models
+
+
+@pytest.mark.django_db
+def test_can_add_player(normal_user):
+    player_endpoint = reverse("home:players-list")
+    client = APIClient()
+    client.force_authenticate(user=normal_user)
+
+    data = {
+        "name": "Test Player"
+    }
+
+    res = client.post(player_endpoint, data)
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data["name"] == data["name"]
+
+
+@pytest.mark.django_db
+def test_cant_see_other_users_players(normal_user, second_user):
+    player_endpoint = reverse("home:players-list")
+    client = APIClient()
+    client.force_authenticate(user=normal_user)
+
+    player_name = "First Player User"
+
+    models.Player.objects.create(
+        name="Second User Player",
+        added_by=second_user
+    )
+
+    models.Player.objects.create(
+        name=player_name,
+        added_by=normal_user
+    )
+
+    player_list = client.get(player_endpoint)
+    assert len(player_list.data) == 1
+    assert player_list.data[0]["name"] == player_name
