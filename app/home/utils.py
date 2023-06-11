a00643698f5301e4dfc319c79f11bcc5eedb7e4a
Josh Bright <josh.bright@gmail.com>
2023-06-11 09:09:34 -0700

    Added function to create hole scores for a game

--- app/home/utils.py
+++ app/home/utils.py
@@ -22,3 +22,24 @@ def get_holes_for_game(course, holes_to_play):
         return hole_list.filter(order__gte=10)
 
     return None
+
+
+def create_hole_scores_for_game(game, holes_to_play):
+    """
+    holes_to_play might be:
+        - 9
+        - front-9
+        - back-9
+        - 18
+    """
+    hole_list = get_holes_for_game(game.course, holes_to_play)
+    if hole_list is None:
+        return False
+
+    for hole in hole_list:
+        for player in game.players.all():
+            game_link = models.PlayerGameLink.objects.filter(
+                player=player, game=game
+            ).first()
+            hole_score = models.HoleScore(hole=hole, game=game_link)
+            hole_score.save()
